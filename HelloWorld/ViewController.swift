20e90b0ce3541ceead0cbef0ac41e5ef93fdf958
eguchi-ap <t.eguchi@alphapolis.co.jp>
2023-07-31 11:19:31 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by WEBSYSTEM-MAC39 on 2023/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
